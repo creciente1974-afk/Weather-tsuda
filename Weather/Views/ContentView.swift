e6dc0e94f71ec254baf5549e00139d3673dc6c53
creciente1974-afk <creciente1974@gmail.com>
2025-11-09 18:22:53 +0900

    位置情報取得できた

--- Weather/Views/ContentView.swift
+++ Weather/Views/ContentView.swift
@@ -5,31 +5,57 @@
 //  Created by tsuda kazumi on 2025/11/09.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @StateObject private var weatherVM = WeatherViewModel()
-    
+    @StateObject var locationManager = LocationManager()  //位置情報管理のオブジェクト
+
     // 八幡平市大更の緯度・経度
     var lat: Double = 39.90246980951984
     var lon: Double = 141.1328515168145
-    
-    
+
     var body: some View {
-        
-        NavigationStack{
+
+        NavigationStack {
             ScrollView {
-                
+
+                VStack(spacing: 20) {
+                    DailyWeatherView(weatherVM: weatherVM)
+                    HourlyWeatherView(weatherVM: weatherVM)
                 }
-            .navigationBarTitle("ここに現在地を表示")//画面上部のタイトル
-            .navigationBarTitleDisplayMode(.inline)//タイトルの表示方法の指定
+                .padding()
+
+            }
+            .navigationTitle("現在地: \(locationManager.address)")
+            .navigationBarTitleDisplayMode(.inline)  //タイトルの表示方法の指定
         }
         .padding()
         .onAppear {
-            weatherVM.request3DaysForecast(lat: lat, lon: lon)
+            getWeatherForecast()
+        }
+        
+        .refreshable {
+            getWeatherForecast()
         }
     }
+
+    // 現在地の天気予報取得、順番に処理したいのでDispatchQueue.main.async使用
+    func getWeatherForecast() {
+        DispatchQueue.main.async {
+            if let location = locationManager.location {
+                let latitude = location.coordinate.latitude  // 緯度
+                let longitude = location.coordinate.longitude  // 軽度
+                weatherVM.request3DaysForecast(lat: latitude, lon: longitude)  // 天気リクエスト
+                print("LAT:", latitude, "LON:", longitude)
+            } else {
+                print("getting weather is failed")
+            }
+        }
+    }
+
 }
 
 #Preview {
